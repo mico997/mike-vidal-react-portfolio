e72d4f30a35de0af4b0f6a4e77d0b5b528b36a01
Mico <mico@Micos-MacBook-Pro.local>
2020-06-18 18:38:46 -0400

    initial commit, 20% done

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-item";
+import axios from "axios";
+
+
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+    this.getPortfolioItems = this.getPortfolioItems.bind(this)
+
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+
+  getPortfolioItems() {
+    axios
+    .get('https://mikekwekam.devcamp.space/portfolio/portfolio_items')
+    .then(response => {
+    // handle success
+    console.log("response data", response);
+    this.setState({
+      data: response.data.portfolio_items
+    })
+  })
+  .catch(response => {
+    // handle error
+    console.log(error);
+  });
+}
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem  key={item.id} title={item.name} url={item.url} slug={item.id} />
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+
+
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+
+
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+
+        {this.portfolioItems()}
+      </div>
+    );
+  }
+}
