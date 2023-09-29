21d556587129e9427097b38b5fe95afce7b4b98d
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2023-09-29 05:45:19 -0400

    Rendered api content on screen and added portfolio form component to dynamically update state. Created inital handle change method for state update

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
   constructor() {
@@ -12,6 +13,19 @@ export default class PortfolioManager extends Component {
     };
 
     this.getPortfolioItems = this.getPortfolioItems.bind(this);
+    this.handleSuccesfulFormSubmission =
+      this.handleSuccesfulFormSubmission.bind(this);
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  }
+
+  handleSuccesfulFormSubmission(portfolioItem) {
+    //TODO
+    //update portfolio item state
+    // and add the portfolioItem to the list
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError error", error);
   }
 
   getPortfolioItems() {
@@ -37,7 +51,10 @@ export default class PortfolioManager extends Component {
     return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <h1>Portfolio Form...</h1>
+          <PortfolioForm
+            handleSuccesfulFormSubmission={this.handleSuccesfulFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
         </div>
 
         <div className="right-column">
