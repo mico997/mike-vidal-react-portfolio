a5dfae78c626475128bac8914de75dee74135e84
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2023-10-03 00:23:44 -0400

    Updated bug causing handlesubmit fail

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -16,13 +16,33 @@ export default class PortfolioManager extends Component {
     this.handleSuccesfulFormSubmission =
       this.handleSuccesfulFormSubmission.bind(this);
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick(portfolioItem) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.setState({
+          portfolioItems: this.state.portfolioItems.filter((item) => {
+            return item.id !== portfolioItem.id;
+          }),
+        });
+
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("handleDeleteClick error", error);
+      });
   }
 
   handleSuccesfulFormSubmission(portfolioItem) {
     this.setState({
       portfolioItems: [portfolioItem].concat(this.state.portfolioItems),
     });
-    console.log("handleSuccesfulFormSubmission", portfolioItem);
   }
 
   handleFormSubmissionError(error) {
@@ -62,7 +82,10 @@ export default class PortfolioManager extends Component {
         </div>
 
         <div className="right-column">
-          <PortfolioSidebarList data={this.state.portfolioItems} />
+          <PortfolioSidebarList
+            handleDeleteClick={this.handleDeleteClick}
+            data={this.state.portfolioItems}
+          />
         </div>
       </div>
     );
