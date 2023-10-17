e8be62ae9c2b485c6b1001790defc9d29cb4628c
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2023-10-17 00:45:54 -0400

    added ability to post blog component with api. fixed filter bug

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -17,22 +17,30 @@ export default class PortfolioContainer extends Component {
   }
 
   handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter((item) => {
-        return item.category === filter;
-      }),
-    });
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
+    }
   }
 
-  getPortfolioItems() {
+  getPortfolioItems(filter = null) {
     axios
       .get("https://mikekwekam.devcamp.space/portfolio/portfolio_items")
       .then((response) => {
         // handle success
-        //console.log("response data", response);
-        this.setState({
-          data: response.data.portfolio_items,
-        });
+        // console.log("response data", response);
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter((item) => {
+              return item.category === filter;
+            }),
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items,
+          });
+        }
       })
       .catch((error) => {
         // handle error
@@ -56,17 +64,31 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div className="portfolio-items-wrapper">
-        <button className="btn" onClick={() => this.handleFilter("ecommerce")}>
-          eCommerce
-        </button>
-        <button className="btn" onClick={() => this.handleFilter("Restaurant")}>
-          Restaurant
-        </button>
-        <button className="btn" onClick={() => this.handleFilter("Travel")}>
-          Travel
-        </button>
-        {this.portfolioItems()}
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("ecommerce")}
+          >
+            eCommerce
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Restaurant")}
+          >
+            Restaurant
+          </button>
+          <button className="btn" onClick={() => this.handleFilter("Travel")}>
+            Travel
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            All
+          </button>
+        </div>
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
       </div>
     );
   }
